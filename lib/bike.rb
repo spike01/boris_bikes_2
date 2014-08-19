97036e49681e82766532233898da8613d5e8cab7
Spike Nox <spike01@gmail.com>
2014-08-19 16:25:45 +0100

    Built bike class

---
+++ lib/bike.rb
@@ -0,0 +1,32 @@
+class Bike
+
+  attr_reader :time, :serial
+
+  def initialize
+    @broken = false
+    @serial = serial_generate
+    @time = nil 
+  end
+
+  def broken?
+    @broken
+  end
+
+  def break!
+    @broken = true
+    self
+  end
+  
+  def fix!
+    @broken = false
+    self
+  end
+
+  def serial_generate
+    (0..9).map { rand(0..9) }.join
+  end
+
+  def time!
+    @time = Time.now.round(0)
+  end
+end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,41 @@
+require 'bike'
+
+describe Bike do
+
+  let(:bike) { Bike.new }
+  
+  context 'Breaking' do
+    it 'is not broken on initialization' do
+      expect(bike.broken?).to be false
+    end
+
+    it 'is able to break' do
+      expect(bike.break!).to be_broken
+    end
+
+    it 'able to be fixed' do
+      expect(bike.break!.fix!).to_not be_broken
+    end
+  end 
+
+  context 'Serial number' do
+    it 'is initialized with a serial number' do
+      expect(bike.serial).not_to eq(nil) 
+    end
+
+    it 'has a unique serial number' do
+      expect(bike.serial).not_to eq(Bike.new.serial)
+    end
+    
+    it 'has a valid serial number' do
+      expect(bike.serial).to match(/\d{9}/)
+    end
+  end
+
+  context 'Time' do
+    it 'can hold a time variable' do
+      bike.time!
+      expect(bike.time).to eq Time.now.round(0) 
+    end
+  end
+end
